10f00b050eb8feb170c4a1d1b4ee7025a224eadd
jordanbrown0606 <157506730+jordanbrown0606@users.noreply.github.com>
2024-03-13 16:47:18 -0500

    Created and implemented door animation and scripts.

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.ProBuilder.Shapes;
 
 public class Interaction : MonoBehaviour
 {
@@ -9,7 +11,41 @@ public class Interaction : MonoBehaviour
 
     void Update()
     {
+        if (_selection != null)
+        {
+            if (_selection.GetComponent<Interactable>() != null)
+            {
+                _selection.GetComponent<Interactable>().HideInteractable();
+            }
+            _selection = null;
+        }
+
         RaycastHit hit;
 
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * 
+            distanceFromObject, Color.red);
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward,
+            out hit, distanceFromObject))
+        {
+            var selection = hit.transform;
+            
+            Debug.Log(selection.gameObject.name);
+
+            if (selection.GetComponent<Interactable>() != null)
+            {
+                selection.GetComponent<Interactable>().DisplayInteractable();
+            }
+
+            if (Mouse.current.leftButton.wasPressedThisFrame)
+            {
+                if (selection.GetComponent<DoorScript>() != null)
+                {
+                    selection.GetComponent<DoorScript>().DoorInteraction();
+                }
+            }
+
+            _selection = selection;
+        }
     }
 }

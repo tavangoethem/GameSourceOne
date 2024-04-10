1c450a985b833f0b955ca4f05f49724b5460cacf
tavangoethem <76858178+tavangoethem@users.noreply.github.com>
2024-04-10 10:14:25 -0500

    dahkjad

--- Assets/Scripts/Weaponry/Rifle.cs
+++ Assets/Scripts/Weaponry/Rifle.cs
@@ -3,140 +3,164 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using Weaponry;
 
-public class Rifle : WeaponBase, IShoot, IReload
+namespace AiStates
 {
-    [SerializeField] private int _damage = 1;
-
-    [SerializeField] private int _maxAmmo;
-
-    [SerializeField] private int _curAmmo;
-
-    [SerializeField] private float _fireRate = .1f;
-
-    private bool _isShoot = false;
-
-    public float recoil = 0;
-
-    private Transform _recoilHelper;
-
-    private bool _running = false;
-
-    public int CurAmmo { get { return _curAmmo; } }
-
-    public int MaxAmmo { get { return _maxAmmo; } }
-
-    public void Shoot(InputAction.CallbackContext obj)
+    public class Rifle : WeaponBase, IShoot, IReload
     {
-        if (obj.started)
-        {
-            StartCoroutine(Shooting());
-        }
-        else if(obj.canceled)
-        {
-            _isShoot = false;
-        }
-    }
+        [SerializeField] private int _damage = 1;
 
+        [SerializeField] private int _maxAmmo;
 
-    private void Update()
-    {
+        [SerializeField] private int _curAmmo;
+
+        [SerializeField] private float _fireRate = .1f;
+
+        private bool _isShoot = false;
+
+        public float recoil = 0;
+
+        private Transform _recoilHelper;
+
+        private bool _running = false;
+
+        public int CurAmmo { get { return _curAmmo; } }
+
+        public int MaxAmmo { get { return _maxAmmo; } }
+
         
-    }
+        [SerializeField, Range(1, 15)] private int SoundOfGun;
 
-    public IEnumerator Shooting()
-    {
-        if (_recoilHelper == null)
+        public void Shoot(InputAction.CallbackContext obj)
         {
-            _recoilHelper = new GameObject("RecoilHelper").transform;
-            _recoilHelper.parent = Camera.main.transform;
-            _recoilHelper.rotation = Camera.main.transform.rotation;
-            
-        }
-
-        _isShoot = true;
-        while (_isShoot && _curAmmo > 0 && _recoilHelper != null)
-        {
-            Transform mainCam = Camera.main.transform;
-
-            _recoilHelper.eulerAngles = new Vector3(_recoilHelper.eulerAngles.x, mainCam.eulerAngles.y, mainCam.eulerAngles.z);
-            _recoilHelper.position = mainCam.position;
-            RaycastHit hit;
-            Ray cameraRay = new Ray(mainCam.position, _recoilHelper.forward);
-            if (Physics.Raycast(cameraRay, out hit))
+            if (obj.started)
             {
-                if (hit.transform.gameObject != null && hit.transform.gameObject.GetComponent<PlayerCharacter>() != true)
-                    hit.transform.gameObject.GetComponent<IDamagable>()?.TakeDamage(_damage, hit.point);
+                StartCoroutine(Shooting());
+                Collider[] colls = Physics.OverlapSphere(this.transform.position, SoundOfGun);
+                foreach (Collider coll in colls)
+                {
+                    if (coll.gameObject.GetComponent<AIStates>() && coll.gameObject.GetComponent<AIStates>().CanSeePlayer == false)
+                    {
+                        coll.gameObject.GetComponent<AIStates>().CanSeePlayer = true;
+                    }
+                }
             }
-            _curAmmo--;
-            recoil = .25f;
-            Recoil();
-            yield return new WaitForSeconds(_fireRate);
+            else if (obj.canceled)
+            {
+                _isShoot = false;
+            }
         }
-        yield return null;
-    }
-
-    public void Reload(InputAction.CallbackContext obj)
-    {
-        _curAmmo = _maxAmmo;
-        recoil = 0f;
-    }
-
-    public override void OnWeaponPickup()
-    {
-        base.OnWeaponPickup();
-        _curAmmo = _maxAmmo;
-        _mainInput.Enable();
-        _mainInput.Player.Enable();
-        _mainInput.Player.Shoot.started += Shoot;
-        _mainInput.Player.Shoot.canceled += Shoot;
-        _mainInput.Player.Reload.performed += Reload;
-    }
-
-    public override void OnWeaponDrop()
-    {
-        base.OnWeaponDrop();
-        _mainInput.Disable();
-        _mainInput.Player.Disable();
-        _mainInput.Player.Shoot.started -= Shoot;
-        _mainInput.Player.Shoot.canceled -= Shoot;
-        _mainInput.Player.Reload.performed -= Reload;
-    }
 
 
-
-    private void Recoil()
-    {        
-        if (recoil > 0)
+        private void Update()
         {
-            var maxRecoil = Quaternion.Euler(-20, 0, 0);
-            // Dampen towards the target rotation
-            _recoilHelper.rotation = Quaternion.Slerp(_recoilHelper.rotation, maxRecoil, Time.deltaTime * 10);
-            transform.localEulerAngles = new Vector3 (_recoilHelper.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z);
-            if(_running == false) StartCoroutine(ResetRecoil());
-        }
-    }
 
-    private IEnumerator ResetRecoil()
-    {
-        _running = true;
-        while(recoil > 0)
+        }
+
+        public IEnumerator Shooting()
         {
-            recoil -= Time.deltaTime;
-            yield return name;
-        }
-        transform.localRotation = Quaternion.identity;
-        Destroy(_recoilHelper.gameObject);
-        _recoilHelper = null;
-        recoil = 0;
-        _running = false;
-    }
+            if (_recoilHelper == null)
+            {
+                _recoilHelper = new GameObject("RecoilHelper").transform;
+                _recoilHelper.parent = Camera.main.transform;
+                _recoilHelper.rotation = Camera.main.transform.rotation;
 
-    private void OnDrawGizmos()
-    {
-        if(_recoilHelper != null)
+            }
+
+            _isShoot = true;
+            while (_isShoot && _curAmmo > 0 && _recoilHelper != null)
+            {
+                Transform mainCam = Camera.main.transform;
+
+                _recoilHelper.eulerAngles = new Vector3(_recoilHelper.eulerAngles.x, mainCam.eulerAngles.y, mainCam.eulerAngles.z);
+                _recoilHelper.position = mainCam.position;
+                RaycastHit hit;
+                Ray cameraRay = new Ray(mainCam.position, _recoilHelper.forward);
+                if (Physics.Raycast(cameraRay, out hit))
+                {
+                    if (hit.transform.gameObject != null && hit.transform.gameObject.GetComponent<PlayerCharacter>() != true)
+                    {
+                        hit.transform.gameObject.GetComponent<IDamagable>()?.TakeDamage(_damage, hit.point);
+                        Collider[] colls1 = Physics.OverlapSphere(hit.transform.position, 5);
+                        foreach (Collider coll in colls1)
+                        {
+                            if (coll.gameObject.GetComponent<AIStates>() && coll.gameObject.GetComponent<AIStates>().CanSeePlayer == false)
+                            {
+                                coll.gameObject.GetComponent<AIStates>().CanSeePlayer = true;
+                            }
+                        }
+                    }
+                }
+                _curAmmo--;
+                recoil = .25f;
+                Recoil();
+                yield return new WaitForSeconds(_fireRate);
+            }
+            yield return null;
+        }
+
+        public void Reload(InputAction.CallbackContext obj)
         {
-            Gizmos.DrawRay(_recoilHelper.position, _recoilHelper.forward);
+            _curAmmo = _maxAmmo;
+            recoil = 0f;
         }
-    }
 
+        public override void OnWeaponPickup()
+        {
+            base.OnWeaponPickup();
+            _curAmmo = _maxAmmo;
+            _mainInput.Enable();
+            _mainInput.Player.Enable();
+            _mainInput.Player.Shoot.started += Shoot;
+            _mainInput.Player.Shoot.canceled += Shoot;
+            _mainInput.Player.Reload.performed += Reload;
+        }
+
+        public override void OnWeaponDrop()
+        {
+            base.OnWeaponDrop();
+            _mainInput.Disable();
+            _mainInput.Player.Disable();
+            _mainInput.Player.Shoot.started -= Shoot;
+            _mainInput.Player.Shoot.canceled -= Shoot;
+            _mainInput.Player.Reload.performed -= Reload;
+        }
+
+
+
+        private void Recoil()
+        {
+            if (recoil > 0)
+            {
+                var maxRecoil = Quaternion.Euler(-20, 0, 0);
+                // Dampen towards the target rotation
+                _recoilHelper.rotation = Quaternion.Slerp(_recoilHelper.rotation, maxRecoil, Time.deltaTime * 10);
+                transform.localEulerAngles = new Vector3(_recoilHelper.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z);
+                if (_running == false) StartCoroutine(ResetRecoil());
+            }
+        }
+
+        private IEnumerator ResetRecoil()
+        {
+            _running = true;
+            while (recoil > 0)
+            {
+                recoil -= Time.deltaTime;
+                yield return name;
+            }
+            transform.localRotation = Quaternion.identity;
+            Destroy(_recoilHelper.gameObject);
+            _recoilHelper = null;
+            recoil = 0;
+            _running = false;
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (_recoilHelper != null)
+            {
+                Gizmos.DrawRay(_recoilHelper.position, _recoilHelper.forward);
+            }
+        }
+
+    }
 }

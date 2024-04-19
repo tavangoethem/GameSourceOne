d16dc87e8f307b1714dc86f15052e009b72a6270
jordanbrown0606 <157506730+jordanbrown0606@users.noreply.github.com>
2024-04-19 16:04:27 -0500

    Adjusted sounds in levels.

--- Assets/Scripts/Interactables/Medkit.cs
+++ Assets/Scripts/Interactables/Medkit.cs
@@ -6,11 +6,13 @@ public class Medkit : MonoBehaviour, IInteractable
 {
     [SerializeField] private int _healAmount = 2;
 
+    [SerializeField] private AudioClip _pickupClip;
 
-    
+
     public void Interact(GameObject objAttemptingInteraction)
     {
         objAttemptingInteraction.GetComponent<IHealable>().HealDamage(_healAmount);
+        AudioManager.instance?.PlaySFX(_pickupClip);
         Destroy(gameObject);
     }
     
